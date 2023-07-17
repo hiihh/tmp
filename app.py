a78f8572bf33754d917e489a17505948b381c414
hiihh <89239386+hiihh@users.noreply.github.com>
2023-07-17 17:26:25 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 from flask import Flask
+from flast import request
+import requests
+import re
+import socket
+
 app = Flask(__name__)
 
+hostname = socket.gethostname()
+hostip = socket.gethostbyname(socket.gethostname())
+
+req = requests.get("http://ipconfig.kr")
+out_ip = re.search(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})', req.text)[1]
+
 @app.route('/')
-def hello_world():
-    return "Hello, GitHub Action"
+def home():
+    return '컴퓨터 이름 : {0}<br>내부 IP : {1}<br>외부 IP: {2} <br><br> 당신의 IP : {3} <br><br>Hello'.format(hostname, hostip, out_ip, request.remote_addr)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
